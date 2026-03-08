811fa7fe34f79b6fd5f50552c3f42bcafc23d94d
Shahriyarl <155835561+Shahriyarl@users.noreply.github.com>
2026-03-08 21:02:44 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,43 +1,43 @@
 from sqlalchemy import create_engine, Column, Integer, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-import sqlite3
+DATABASE_URL = "sqlite:///./solar.db"
 
-conn = sqlite3.connect("solar.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS energy (
-    time INTEGER,
-    solar REAL,
-    battery REAL
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
-""")
 
-conn.commit()
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+class Energy(Base):
+    __tablename__ = "energy"
+
+    id = Column(Integer, primary_key=True, index=True)
+    time = Column(Integer)
+    solar = Column(Float)
+    battery = Column(Float)
+
+
+Base.metadata.create_all(bind=engine)
 
 
 def save_data(time, solar, battery):
+    db = SessionLocal()
 
-    cursor.execute(
-        "INSERT INTO energy VALUES (?, ?, ?)",
-        (time, solar, battery)
+    data = Energy(
+        time=time,
+        solar=solar,
+        battery=battery
     )
 
-    conn.commit()
-
-
-engine = create_engine("sqlite:///solar.db")
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
-
-class SolarData(Base):
-    __tablename__ = "solar_data"
-
-    id = Column(Integer, primary_key=True)
-    hour = Column(Integer)
-    solar_power = Column(Float)
-    battery = Column(Float)
-    load = Column(Float)
-
-Base.metadata.create_all(engine)
+    db.add(data)
+    db.commit()
+    db.close()
