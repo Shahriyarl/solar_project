591a87823f27eb89e1d41711412f682dbc26cfe8
Shahriyarl <155835561+Shahriyarl@users.noreply.github.com>
2026-03-08 21:09:41 +0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,43 @@
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, Float
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-def save_data(voltage, current, power):
+DATABASE_URL = "sqlite:///./solar.db"
 
-    conn = sqlite3.connect("data.db")
-    cursor = conn.cursor()
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-    cursor.execute(
-        "INSERT INTO solar_data (voltage, current, power) VALUES (?, ?, ?)",
-        (voltage, current, power)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+
+class Energy(Base):
+    __tablename__ = "energy"
+
+    id = Column(Integer, primary_key=True, index=True)
+    time = Column(Integer)
+    solar = Column(Float)
+    battery = Column(Float)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def save_data(time, solar, battery):
+    db = SessionLocal()
+
+    data = Energy(
+        time=time,
+        solar=solar,
+        battery=battery
     )
 
-    conn.commit()
-    conn.close()
+    db.add(data)
+    db.commit()
+    db.close()
